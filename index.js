fb887a59af37c736502c1c3cd46ef18e8e386d48
Antonio <antonio.gomez9426mail.com>
2025-12-09 22:16:34 -0600

    Java scrip

---
+++ index.js
@@ -0,0 +1,14 @@
+const fn = () => {
+        const mistring = "soy un string!"
+    let b = mistring = "muy feliz"
+    console.log(b) 
+    console.log('hola mundo')   
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "chanchito feliz"
+
+    console.log
+}
